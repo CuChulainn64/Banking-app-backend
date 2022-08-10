b0cb02c35ac9b0441a9adabda68aed4109fc75f0
CuChulainn64 <patrick.miller1@snhu.edu>
2022-08-10 13:22:26 -0400

    finished transaction history model

--- src/main/java/com/revature/models/Transaction.java
+++ src/main/java/com/revature/models/Transaction.java
@@ -1,6 +1,7 @@
 package com.revature.models;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,6 +10,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.revature.util.TransactionType;
@@ -33,11 +36,105 @@ public class Transaction {
 	@Column(name="time")
 	private LocalDateTime time;
 	
-	@Column(name="source")//fk can be null
+	@ManyToOne
+	@JoinColumn(name = "source_account", nullable = true, table="accounts")
 	private long sourceAccount;
 	
-	@Column(name="target")//fk must not be null
+	@ManyToOne
+	@JoinColumn(name = "target_accountI", nullable = false, table="accounts")
 	private long targetAccount;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public TransactionType getType() {
+		return type;
+	}
+
+	public void setType(TransactionType type) {
+		this.type = type;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
+
+	public long getSourceAccount() {
+		return sourceAccount;
+	}
+
+	public void setSourceAccount(long sourceAccount) {
+		this.sourceAccount = sourceAccount;
+	}
+
+	public long getTargetAccount() {
+		return targetAccount;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(amount, id, sourceAccount, targetAccount, time, type);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transaction other = (Transaction) obj;
+		return Double.doubleToLongBits(amount) == Double.doubleToLongBits(other.amount) && id == other.id
+				&& sourceAccount == other.sourceAccount && targetAccount == other.targetAccount
+				&& Objects.equals(time, other.time) && type == other.type;
+	}
+
+	public Transaction() {
+		super();
+	}
+
+	public Transaction(TransactionType type, double amount, LocalDateTime time, long sourceAccount,
+			long targetAccount) {
+		super();
+		this.type = type;
+		this.amount = amount;
+		this.time = time;
+		this.sourceAccount = sourceAccount;
+		this.targetAccount = targetAccount;
+	}
+
+	public Transaction(long id, TransactionType type, double amount, LocalDateTime time, long sourceAccount,
+			long targetAccount) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.amount = amount;
+		this.time = time;
+		this.sourceAccount = sourceAccount;
+		this.targetAccount = targetAccount;
+	}
+
+	public void setTargetAccount(long targetAccount) {
+		this.targetAccount = targetAccount;
+	}
 	
 	
 }
